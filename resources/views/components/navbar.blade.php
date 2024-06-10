8cfbbcd6a9a900379bb6415d51e68bbdee4824bd
Nabil Hardiansa <nhardiansa@gmail.com>
2024-06-10 17:36:38 +0800

    feat: custom navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg fixed-top bg-white">
+  <div class="container">
+    <a class="navbar-brand" href="/">
+      <img src="{{ asset('images/SBI-logo.png') }}" alt="Sakti Biru Indonesia">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      {{-- <span class="navbar-toggler-icon"></span> --}}
+      <i class="menu bi bi-list"></i>
+      <i class="close bi bi-x-lg hide"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ms-auto">
+        <a class="nav-link" href="#">Home</a>
+        <a class="nav-link" href="#">Our Services</a>
+        <a class="nav-link" href="#">Products</a>
+        <a class="nav-link" href="#">Blog and News</a>
+        <a class="nav-link" href="#">Contacts</a>
+      </div>
+    </div>
+  </div>
+</nav>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,13 +8,29 @@
     @yield('title', 'Sakti Biru Indonesia')
   </title>
 
+  {{-- Bootstrap CSS --}}
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+  {{-- Custom CSS --}}
+  <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
+  {{-- Font --}}
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
 </head>
 
 <body>
 
-  @yield('content')
+  @include('components.navbar')
+
+  <div class="content-wrapper container">
+    @yield('content')
+  </div>
 
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
     integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
@@ -23,6 +39,22 @@
     integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
   </script>
 
+  <script>
+    const contentWrapper = document.querySelector('.content-wrapper');
+    const navbar = document.querySelector('nav.navbar');
+
+    const navbarBtn = navbar.querySelector('button.navbar-toggler');
+    const menuBtnIcon = navbarBtn.querySelector('i.menu');
+    const closeBtnIcon = navbarBtn.querySelector('i.close');
+
+    contentWrapper.style.paddingTop = navbar.offsetHeight + 'px';
+
+    navbarBtn.addEventListener('click', () => {
+      menuBtnIcon.classList.toggle('hide');
+      closeBtnIcon.classList.toggle('hide');
+    });
+
+  </script>
   @yield('script')
 </body>
 
