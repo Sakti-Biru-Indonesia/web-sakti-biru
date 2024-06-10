c25b1d4ccbc69fd05af941577c02faeb6ef95c97
Nabil Hardiansa <nhardiansa@gmail.com>
2024-06-10 23:51:06 +0800

    feat: footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,71 @@
+<footer>
+  <div class="container">
+
+    <div class="footer-content d-flex justify-content-between gap-4">
+
+      {{-- Simple Description --}}
+      <div class="simple-description">
+        <img src="{{ asset('images/SBI-logo.png') }}" alt="logo-SBI">
+        <p class="m-0 mt-3">
+          Early stage startup bidang akuakultur/perikanan dengan peran sebagai enabler atau end-to-end di industri
+          udang.
+        </p>
+      </div>
+
+      {{-- Services --}}
+      <div class="services">
+        <h2>
+          Sakti Biru Indonesia
+        </h2>
+        <div class="services-list d-flex flex-column mt-4">
+          <a href="#">Input and Technology Supplier</a>
+          <a href="#">Farm Management</a>
+          <a href="#">Consultation</a>
+          <a href="#">Cold Storage and Processing</a>
+        </div>
+      </div>
+
+      {{-- Contacts --}}
+      <div class="contact">
+        <h2 class="">Our Contact</h2>
+        <p>
+          8561 Kozey Union Lake Josechester 20830 United States
+        </p>
+        <div class="telp d-flex align-items-center gap-2 mt-2">
+          <p>
+            (300) 502-2905 x4342
+          </p>
+          <i class="fas fa-phone"></i>
+        </div>
+        <div class="social-media d-flex justify-content-between">
+          <a href="#">
+            <i class="bi bi-tiktok"></i>
+          </a>
+          <a href="#">
+            <i class="bi bi-instagram"></i>
+          </a>
+          <a href="#">
+            <i class="bi bi-facebook"></i>
+          </a>
+          <a href="#">
+            <i class="bi bi-linkedin"></i>
+          </a>
+          <a href="#">
+            <i class="bi bi-envelope"></i>
+          </a>
+        </div>
+      </div>
+
+    </div>
+
+    {{-- Copyright Section --}}
+    <div class="copyright d-flex justify-content-between align-items-center">
+      <p class="copyright-text m-0">&copy; 2024 Sakti Biru Indonesia</p>
+      <div class="other-list d-flex gap-4">
+        <a href="#">Privacy Policy</a>
+        <a href="#">Terms of Service</a>
+        <a href="#">Created by Sakti Biru Indonesia</a>
+      </div>
+    </div>
+  </div>
+</footer>
