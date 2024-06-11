f7691f453080c21945b7e83664116045fdd63391
Nabil Hardiansa <nhardiansa@gmail.com>
2024-06-11 14:04:00 +0800

    feat: hero section in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,26 @@
 @extends('layout')
 
 @section('content')
-<h1>
-  asdasdasd
-</h1>
+{{-- Display Hero --}}
+
+<section class="display-hero">
+  <div class="content">
+    <h1>
+      <span>Indonesia’s</span> Trusted Partner
+    </h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur. Tincidunt eleifend dignissim molestie fringilla lobortis
+    </p>
+    <button class="btn btn-primary rounded-pill py-3 px-4">
+      Talk to Us
+    </button>
+  </div>
+  <div class="graphics">
+    <img class="bg-wave" src="{{ asset('images/background-wave.png') }}" alt="background-wave">
+    <img class="sea" src="{{ asset('images/sea.jpg') }}" alt="sea">
+    <img class="fishery" src="{{ asset('images/fishery.png') }}" alt="fishery">
+    <img class="round-waves" src="{{ asset('images/ic_round-waves.svg') }}" alt="round-waves">
+  </div>
+</section>
+
 @endsection
