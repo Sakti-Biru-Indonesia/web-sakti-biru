d2d89643a8078161ff34b0d6f22d45d066acbd95
Nabil Hardiansa <nhardiansa@gmail.com>
2024-06-15 14:04:44 +0800

    fix: put route home

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg fixed-top bg-white">
   <div class="container">
-    <a class="navbar-brand" href="/">
+    <a class="navbar-brand" href="{{ route('home') }}">
       <img src="{{ asset('images/SBI-logo.png') }}" alt="Sakti Biru Indonesia">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
@@ -11,7 +11,7 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav ms-auto">
-        <a class="nav-link" href="#">Home</a>
+        <a class="nav-link" href="{{ route('home') }}">Home</a>
         <a class="nav-link" href="#">Our Services</a>
         <a class="nav-link" href="#">Products</a>
         <a class="nav-link" href="{{ route('blog-news') }}">Blog and News</a>
