1251d4ab1aa292c866c8948174f348aa9b434844
Nabil Hardiansa <nhardiansa@gmail.com>
2024-06-15 10:33:42 +0800

    feat: add writer list and search input

--- resources/views/blog-news.blade.php
+++ resources/views/blog-news.blade.php
@@ -5,6 +5,7 @@ Blog and News
 @endsection
 
 @section('content')
+
 <section class="featured-news">
   <div class="heading">
     <h2 class="fw-bold font-outfit">
@@ -19,7 +20,8 @@ Blog and News
           SCI’s strategic recommendations for Indonesian shrimp industry challenges
         </h3>
         <p class="headline-description font-open-sans fw-normal">
-          SCI emphasized two key issues during the meeting: a decline in shrimp production in 2023 and an ongoing trend
+          SCI emphasized two key issues during the meeting: a decline in shrimp production in 2023 and an ongoing
+          trend
           of
           decreasing prices.
         </p>
@@ -98,6 +100,86 @@ Blog and News
       </div>
     </div>
   </div>
+</section>
+
+<section class="explore-news">
+  <div class="row">
+
+    {{-- Explore News List --}}
+    <div class="explore-news-list col d-flex flex-column">
+
+      {{-- Search Input Form --}}
+      <form action="#" method="get" class="search-input-form">
+        <div class="input-group position-relative">
+          <input type="text" class="search-input form-control rounded-pill"
+            placeholder="Find news, articles, or blog posts.." aria-label="search-input" aria-describedby="search">
+          <button class="search-btn btn position-absolute" type="submit" id="search">
+            <i class="bi bi-search"></i>
+          </button>
+        </div>
+      </form>
+
+      {{-- Explore News List Group --}}
+      <div class="explore-news-list-group">
+        <div class="explore-news-group-item">
+
+        </div>
+      </div>
+    </div>
+
+    {{-- Author List --}}
+    <div class="author-list col d-flex flex-column align-items-end">
+      <h3 class="author-list-header-title font-outfit font-medium">
+        Our Writers
+      </h3>
+      <div class="author-list-wrapper d-flex flex-column">
+        <a href="#" class="author-list-item d-flex align-items-center">
+          <div class="author-list-item-text d-flex flex-column align-items-end">
+            <p class="author-list-item-name font-outfit text-end font-medium">
+              Justin Knoll
+            </p>
+            <span class="author-list-item-role font-outfit fw-normal text-end">
+              Commodities Expert
+            </span>
+          </div>
+          <img class="rounded-circle" src="{{ asset('images/author-placeholder.png') }}" alt="author">
+        </a>
+        <a href="#" class="author-list-item d-flex align-items-center">
+          <div class="author-list-item-text d-flex flex-column align-items-end">
+            <p class="author-list-item-name font-outfit text-end font-medium">
+              Justin Knoll
+            </p>
+            <span class="author-list-item-role font-outfit fw-normal text-end">
+              Commodities Expert
+            </span>
+          </div>
+          <img class="rounded-circle" src="{{ asset('images/author-placeholder.png') }}" alt="author">
+        </a>
+        <a href="#" class="author-list-item d-flex align-items-center">
+          <div class="author-list-item-text d-flex flex-column align-items-end">
+            <p class="author-list-item-name font-outfit text-end font-medium">
+              Justin Knoll
+            </p>
+            <span class="author-list-item-role font-outfit fw-normal text-end">
+              Commodities Expert
+            </span>
+          </div>
+          <img class="rounded-circle" src="{{ asset('images/author-placeholder.png') }}" alt="author">
+        </a>
+        <a href="#" class="author-list-item d-flex align-items-center">
+          <div class="author-list-item-text d-flex flex-column align-items-end">
+            <p class="author-list-item-name font-outfit text-end font-medium">
+              Justin Knoll
+            </p>
+            <span class="author-list-item-role font-outfit fw-normal text-end">
+              Commodities Expert
+            </span>
+          </div>
+          <img class="rounded-circle" src="{{ asset('images/author-placeholder.png') }}" alt="author">
+        </a>
+      </div>
+    </div>
   </div>
 </section>
+
 @endsection
