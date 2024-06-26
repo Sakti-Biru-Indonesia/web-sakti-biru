e61506ea6c2a527f70a56be14790ba16ff429b57
Nabil Hardiansa <nhardiansa@gmail.com>
2024-06-27 00:03:17 +0800

    fix: add internalization in navbar dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -18,10 +18,18 @@
   <link
     href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
     rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+    rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
 
-  <!-- Custom styles for this template-->
+  <!-- Styles for this template-->
   <link href="{{ asset('/css/dashboard/sb-admin-2.min.css') }}" rel="stylesheet">
+
+  <!-- Custom Styles for this template-->
+  <link href="{{ asset('/css/custom-dashboard.css') }}" rel="stylesheet">
 
   {{-- Favicon --}}
   <link rel="shortcut icon" href="{{asset('images/SBI-logo.png')}}" type="image/x-icon">
