9a0046bb059597782ca54431db1bd579e4df6368
Fahreza17 <indminecraft52@gmail.com>
2024-06-26 08:30:46 +0700

    Added Indonesian translations for home page

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,5 +2,12 @@
 return [
     'welcome' => 'Welcome to our website!',
     'dashboard' => 'Dashboard',
-    // tambahkan semua teks yang perlu diterjemahkan
+    'our_services' => 'Our Services',
+    'input_technology_supplier' => 'Input & Technology Supplier',
+    'farm_management' => 'Farm Management',
+    'consultation' => 'Consultation',
+    'cold_storage_processing' => 'Cold Storage & Processing ',
+    'expert_writers_leading_analysis' => 'Our Expert Writers, Brings You The Leading Analysis',
+    'world_leading_aquaculture_products'=> 'Our World Leading Aquaculture Products ',
+    'still_have_questions_reach_out' => 'Still have questions? Reach out to us..',
 ];
