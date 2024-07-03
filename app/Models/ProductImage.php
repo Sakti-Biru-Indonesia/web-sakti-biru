a54fa64897fad88275044acf244c2f096bf41ace
Nabil Hardiansa <nhardiansa@gmail.com>
2024-07-03 11:38:50 +0800

    fix: make seperate model for details of product

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -4,15 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class ProductImage extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $fillable = ['product_id', 'url'];
+  protected $fillable = ['product_id', 'url'];
 
-    public function product()
-    {
-        return $this->belongsTo(Product::class);
+  public function product()
+  {
+    return $this->belongsTo(Product::class);
+  }
+
+  public function deleteImage($path)
+  {
+    if (Storage::exists($path)) {
+      Storage::delete(str_replace('storage', 'public', $path));
     }
+  }
 }
