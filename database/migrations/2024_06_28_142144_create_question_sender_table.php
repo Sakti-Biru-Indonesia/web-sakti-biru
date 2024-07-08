52e570cc8ac56bc1024bb7405e48eb48dffaec8a
Nabil Hardiansa <nhardiansa@gmail.com>
2024-07-08 13:17:13 +0800

    fix: typo question sender table

--- database/migrations/2024_06_28_142144_create_question_sender_table.php
+++ database/migrations/2024_06_28_142144_create_question_sender_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateQuestionSenderTable extends Migration
 {
-    public function up()
-    {
-        Schema::create('question_senders', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('phone_number');
-            $table->text('message');
-            $table->timestamps();
-        });
-    }
+  public function up()
+  {
+    Schema::create('question_senders', function (Blueprint $table) {
+      $table->id();
+      $table->string('name');
+      $table->string('phone_number');
+      $table->text('message');
+      $table->timestamps();
+    });
+  }
 
-    public function down()
-    {
-        Schema::dropIfExists('question_sender');
-    }
+  public function down()
+  {
+    Schema::dropIfExists('question_senders');
+  }
 }
-
